509ffa54dac5abc579e0d00a237569daa76e0e65
Silas <silas@level2d.com>
2019-02-22 09:28:09 -0500

    changes to deploy to hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule CloudfrontSigner.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
     ]
   end
@@ -21,7 +23,21 @@ defmodule CloudfrontSigner.MixProject do
   defp deps do
     [
       {:poison, "~> 3.1"},
-      {:timex, "~> 3.1"}
+      {:timex, "~> 3.1"},
+      {:ex_doc, "~> 0.14", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Elixir implementation of Cloudfront's url signature algorithm."
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib config .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Poeticode/cloudfront-signer"}
     ]
   end
 end
