613d73b2251ab1074412d1faaf4c3402d2bd5e43
Xin Fan <xin@teamapp.com>
2022-01-11 13:07:22 +1100

    Use the new child spec for Supervisor

--- lib/cloudfront_signer/application.ex
+++ lib/cloudfront_signer/application.ex
@@ -1,10 +1,9 @@
 defmodule CloudfrontSigner.Application do
   use Application
-  import Supervisor.Spec
 
   def start(_type, _args) do
     children = [
-      worker(CloudfrontSigner.DistributionRegistry, [])
+      {CloudfrontSigner.DistributionRegistry, []}
     ]
     opts = [strategy: :one_for_one, name: CloudfrontSigner.Application.Supervisor]
     Supervisor.start_link(children, opts)
